e9778748c31a484c9b5e871719d5e4ca44fadea7
Chris Weiskopf <chris@chriscancode.io>
2023-07-02 20:03:53 -0400

    adding some console.logs

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,8 +1,12 @@
 export async function load({ fetch }) {
-  const res = await fetch("http://localhost:3000/api/posts", {
-    credentials: "include",
-  });
+  const res = await fetch(
+    "https://kindness-server-production.up.railway.app/api/posts",
+    {
+      credentials: "include",
+    }
+  );
   const posts = await res.json();
+  console.log(posts[0].message);
   return {
     posts,
   };
